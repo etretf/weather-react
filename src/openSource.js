d53e089d339b9a1a0193be31edeb0da268b097d8
etretf <77692774+etretf@users.noreply.github.com>
2021-03-21 14:56:39 -0400

    Removed some searchBar.js code, added styles.css

--- src/openSource.js
+++ src/openSource.js
@@ -5,7 +5,7 @@ export default function openSource(){
   return(
   <div className="ocSentence">
         <span>
-          <a className="opensc" href="https://github.com/etretf/weather-app">Open Source Code </a><span>by Emma Souannhaphanh</span>
+          <a className="opensc" href="https://github.com/etretf/weather-react">Open Source Code </a><span>by Emma Souannhaphanh</span>
         </span>
   </div>
   )
--- src/searchBar.js
+++ src/searchBar.js
@@ -1,24 +1,15 @@
-import React, { useState } from "react";
 import "./searchBar.css";
 
 export default function searchBar() {
-  let [city, setCity] = useState("");
-
+  
   function handleSubmit(event) {
     event.preventDefault();
-    if (city.length>0) {alert(`It is 15°C in ${city}`)}
-    else {alert("Enter a city")};
-  }
-
-  function updateCity(event) {
-    event.preventDefault();
-    setCity(event.target.value);
   }
 
   return (
     <div className="searchBar">
        <form onSubmit={handleSubmit}>
-      <input type="text" onChange={updateCity} />
+      <input type="text"/>
       <input type="submit" value="search" className="searchButton"/>
     </form>
     </div>
